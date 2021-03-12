5fe558a060851060e615f329669166b29e8e2ade
Lorenzo <lorenzo.zem8065@mediadesign.school.nz>
2021-03-12 15:03:07 +1300

    Initial Commit

---
+++ EggTimer-Programmatic2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EggTimer-Programmatic2
+//
+//  Created by Lorenzo Zemp on 12/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
