427fa2bedc2fa22e6eede6da49599dc4c1659066
Lorenzo <lorenzo.zem8065@mediadesign.school.nz>
2021-03-16 00:14:54 +1300

    Added progressBar UIProgressView object and Vertical Stack View

--- EggTimer-Programmatic2/TimerVC.swift
+++ EggTimer-Programmatic2/TimerVC.swift
@@ -11,6 +11,8 @@ class TimerVC: UIViewController {
 
     var titleLabel = UILabel()
     var horizontalStackView = UIStackView()
+    var verticalStackView = UIStackView()
+    var progressBar = UIProgressView()
         
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,41 +25,33 @@ class TimerVC: UIViewController {
         
         //confige views
         configureTitleLabel()
+        configureProgressView()
         
         //config stack views
-        configureStackView()
+        configureHStackView()
+        configureVStackView()
     }
     
     func configureTitleLabel(){
-        view.addSubview(titleLabel)
-        
         titleLabel.text = "How would you like your eggs?"
         titleLabel.font = .systemFont(ofSize: 25, weight: .semibold)
         titleLabel.textAlignment = .center
         titleLabel.lineBreakMode = .byWordWrapping
-        
-        titleLabel.translatesAutoresizingMaskIntoConstraints = false;
-        NSLayoutConstraint.activate([
-            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            titleLabel.centerYAnchor.constraint(equalTo: view.topAnchor, constant: 250)
-        ])
     }
     
-    func configureStackView() {
-        view.addSubview(horizontalStackView)
-        
+    func configureHStackView() {
         horizontalStackView.axis = .horizontal
         horizontalStackView.spacing = 5
         horizontalStackView.backgroundColor = .systemTeal
         horizontalStackView.distribution = .fillEqually
         
-        horizontalStackView.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            horizontalStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            horizontalStackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            horizontalStackView.widthAnchor.constraint(equalToConstant: view.frame.width),
-            horizontalStackView.heightAnchor.constraint(equalToConstant: 300)
-        ])
+//        horizontalStackView.translatesAutoresizingMaskIntoConstraints = false
+//        NSLayoutConstraint.activate([
+//            horizontalStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+//            horizontalStackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+//            horizontalStackView.widthAnchor.constraint(equalToConstant: view.frame.width),
+//            horizontalStackView.heightAnchor.constraint(equalToConstant: 300)
+//        ])
         
         let softEggIV = UIImageView(image: #imageLiteral(resourceName: "soft_egg"))
         softEggIV.contentMode = .scaleAspectFit
@@ -72,5 +66,30 @@ class TimerVC: UIViewController {
         horizontalStackView.addArrangedSubview(softEggIV)
         horizontalStackView.addArrangedSubview(mediumEggIV)
         horizontalStackView.addArrangedSubview(hardEggIV)
+    }
+    
+    func configureProgressView() {
+        progressBar.progress = 0.5
+    }
+    
+    func configureVStackView() {
+        view.addSubview(verticalStackView)
+        
+        verticalStackView.axis = .vertical
+        verticalStackView.distribution = .fillEqually
+        verticalStackView.backgroundColor = .systemYellow
+        
+        verticalStackView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            verticalStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            verticalStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            verticalStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            verticalStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+        ])
+        
+        verticalStackView.addArrangedSubview(titleLabel)
+        verticalStackView.addArrangedSubview(horizontalStackView)
+        //add progress bar
+        verticalStackView.addArrangedSubview(progressBar)
     }
 }
